78222606a167ac03365c21adaa46a33d9f7e7eaf
lukendev <lucas.azevedo2104@gmail.com>
2022-09-20 20:07:44 -0300

    Desafio Número Aleatório

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from "react";
 
+import Aleatorio from "./components/basicos/Aleatorio";
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
@@ -8,6 +9,7 @@ import Fragmento from './components/basicos/Fragmento'
 export default _ =>
     <div>
         <h1>Fundamentos React</h1>
+        <Aleatorio min={1} max={60} />
         <Fragmento></Fragmento>
         <ComParametro
             titulo="Situação"
