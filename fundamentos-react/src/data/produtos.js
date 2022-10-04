041755533d80be5051364ca7739ac2c216c8ed3d
lukendev <lucas.azevedo2104@gmail.com>
2022-10-04 18:09:55 -0300

    Desafio Repetição

---
+++ fundamentos-react/src/data/produtos.js
@@ -0,0 +1,14 @@
+const produtos = [
+    { id: 1, nome: 'Caneta', preco: 2.99 },
+    { id: 1, nome: 'Lapis', preco: 1.99 },
+    { id: 1, nome: 'iPad', preco: 5899.99 },
+    { id: 1, nome: 'Samsung S20 Ultra', preco: 2300.99 },
+    { id: 1, nome: 'Notebook', preco: 3409.99 },
+    { id: 1, nome: 'Caderno', preco: 19.99 },
+    { id: 1, nome: 'Borracha', preco: 4.99 },
+    { id: 1, nome: 'Impressora', preco: 889.99 },
+    { id: 1, nome: 'Monitor 27', preco: 799.993432 },
+    { id: 1, nome: 'Cadeira', preco: 1239.99 }
+]
+
+export default produtos
